019dd40eb6ab2fc150284ce88039e2ce3d3f742b
el <elrophilskwaila@gmail.com>
2021-02-20 22:27:02 +0300

    app.js resolved to proper formular

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ var fNames = ['Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama'];
 function getData(){
     event.preventDefault();
     var date = document.getElementById('date').value;
-    var gender1 = document.getElementById('gender1').value;
-    var gender2 = document.getElementById('gender2').value;
+    var gender = document.getElementById('gender').value;
+    // var gender2 = document.getElementById('gender2').value;
+    console.log(gender)
 
     //getdate and pick day
     let fdate = new Date(date).toDateString();
@@ -16,60 +17,60 @@ function getData(){
     document.getElementById('result').innerHTML = mdate;
 
 
-    if(mdate == days[0] && gender2){
+    if(mdate == days[0] && gender == 2){
         console.log(mNames[0]);
          var name = mNames[0];
          document.getElementById("result1").innerHTML= name;
-    }else if (mdate == days[1] && gender2){
+    }else if (mdate == days[1] && gender == 2){
         console.log(mNames[1]);
         var name1 = mNames[1];
          document.getElementById("result1").innerHTML= name1;
-    }else if (mdate == days[2] && gender2){
+    }else if (mdate == days[2] && gender == 2){
         console.log(mNames[2]);
         var name2 = mNames[2];
         document.getElementById("result1").innerHTML=name2;
-    }else if (mdate == days[3] && gender2){
+    }else if (mdate == days[3] && gender == 2){
         console.log(mNames[3]);
         var name3 = mNames[3];
         document.getElementById('result1').innerHTML=name3;
-    }else if (mdate == days[4] && gender2){
+    }else if (mdate == days[4] && gender == 2){
         console.log(mNames[4]);
         var name4 =mNames[4];
         document.getElementById('result1').innerHTML=name4;
-    }else if (mdate == days[5] && gender2){
+    }else if (mdate == days[5] && gender == 2){
         console.log(mNames[5]);
         var name5 =mNames[5];
         document.getElementById('result').innerHTML=name5;
-    }else if (mdate == days[6] && gender2){
+    }else if (mdate == days[6] && gender == 2){
         console.log(mNames[6]);
         var name6 =mNames[6];
         document.getElementById('result1').innerHTML=name6;
     }
-      if(mdate == days[0] && gender1){
+      if(mdate == days[0] && gender == 1){
         console.log(fNames[0]);
         var fname = fNames[0];
         document.getElementById("result1").innerHTML= fname;
-    }else if (mdate == days[1] && gender1){
+    }else if (mdate == days[1] && gender == 1){
         console.log(fNames[1]);
         var fname1 = fNames[1];
         document.getElementById("result1").innerHTML= fname1;
-    }else if (mdate == days[2] && gender1){
+    }else if (mdate == days[2] && gender == 1){
         console.log(fNames[2]);
         var fname2 = fNames[2];
         document.getElementById("result1").innerHTML= fname2;
-    }else if (mdate == days[3] && gender1){
+    }else if (mdate == days[3] && gender == 1){
         console.log(fNames[3]);
         var fname3 = fNames[3];
         document.getElementById("result1").innerHTML= fname3;
-    }else if (mdate == days[4] && gender1){
+    }else if (mdate == days[4] && gender == 1){
         console.log(fNames[4]);
         var fname4 = fNames[4];
         document.getElementById("result1").innerHTML= fname4;
-    }else if (mdate == days[5] && gender1){
+    }else if (mdate == days[5] && gender == 1){
         console.log(fNames[5]);
         var fname5 = fNames[5];
         document.getElementById("result1").innerHTML= fname5;
-    }else if (mdate == days[6] && gender1){
+    }else if (mdate == days[6] && gender == 1){
         console.log(fNames[6]);
         var fname6 = fNames[6];
         document.getElementById("result1").innerHTML= fname6;
