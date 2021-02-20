64c71cb76c5ab66037454e151ac67e9419f25f3d
el <elrophilskwaila@gmail.com>
2021-02-20 22:49:37 +0300

    clear form after submit

--- app.js
+++ app.js
@@ -21,59 +21,88 @@ function getData(){
         console.log(mNames[0]);
          var name = mNames[0];
          document.getElementById("result1").innerHTML= name;
+         document.getElementById('date').value = '';
+         document.getElementById('gender').value = '';
     }else if (mdate == days[1] && gender == 2){
         console.log(mNames[1]);
         var name1 = mNames[1];
          document.getElementById("result1").innerHTML= name1;
+         document.getElementById('date').value = '';
+         document.getElementById('gender').value = '';
     }else if (mdate == days[2] && gender == 2){
         console.log(mNames[2]);
         var name2 = mNames[2];
         document.getElementById("result1").innerHTML=name2;
+         document.getElementById('date').value = '';
+         document.getElementById('gender').value = '';
     }else if (mdate == days[3] && gender == 2){
         console.log(mNames[3]);
         var name3 = mNames[3];
         document.getElementById('result1').innerHTML=name3;
+         document.getElementById('date').value = '';
+         document.getElementById('gender').value = '';
     }else if (mdate == days[4] && gender == 2){
         console.log(mNames[4]);
         var name4 =mNames[4];
         document.getElementById('result1').innerHTML=name4;
+         document.getElementById('date').value = '';
+         document.getElementById('gender').value = '';
     }else if (mdate == days[5] && gender == 2){
         console.log(mNames[5]);
         var name5 =mNames[5];
         document.getElementById('result').innerHTML=name5;
+         document.getElementById('date').value = '';
+         document.getElementById('gender').value = '';
     }else if (mdate == days[6] && gender == 2){
         console.log(mNames[6]);
         var name6 =mNames[6];
         document.getElementById('result1').innerHTML=name6;
+         document.getElementById('date').value = '';
+         document.getElementById('gender').value = '';
     }
       if(mdate == days[0] && gender == 1){
         console.log(fNames[0]);
         var fname = fNames[0];
         document.getElementById("result1").innerHTML= fname;
+         document.getElementById('date').value = '';
+         document.getElementById('gender').value = '';
     }else if (mdate == days[1] && gender == 1){
         console.log(fNames[1]);
         var fname1 = fNames[1];
         document.getElementById("result1").innerHTML= fname1;
+         document.getElementById('date').value = '';
+         document.getElementById('gender').value = '';
     }else if (mdate == days[2] && gender == 1){
         console.log(fNames[2]);
         var fname2 = fNames[2];
         document.getElementById("result1").innerHTML= fname2;
+         document.getElementById('date').value = '';
+         document.getElementById('gender').value = '';
     }else if (mdate == days[3] && gender == 1){
         console.log(fNames[3]);
         var fname3 = fNames[3];
         document.getElementById("result1").innerHTML= fname3;
+         document.getElementById('date').value = '';
+         document.getElementById('gender').value = '';
     }else if (mdate == days[4] && gender == 1){
         console.log(fNames[4]);
         var fname4 = fNames[4];
         document.getElementById("result1").innerHTML= fname4;
+         document.getElementById('date').value = '';
+         document.getElementById('gender').value = '';
     }else if (mdate == days[5] && gender == 1){
         console.log(fNames[5]);
         var fname5 = fNames[5];
         document.getElementById("result1").innerHTML= fname5;
+         document.getElementById('date').value = '';
+         document.getElementById('gender').value = '';
     }else if (mdate == days[6] && gender == 1){
         console.log(fNames[6]);
         var fname6 = fNames[6];
         document.getElementById("result1").innerHTML= fname6;
+         document.getElementById('date').value = '';
+         document.getElementById('gender').value = '';
     }
+
  }
 
